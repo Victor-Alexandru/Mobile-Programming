059747830c51ca30bfd13bb2d0e27aa1236cd7b6
unknown <victor21cuciureanu@gmail.com>
2019-11-11 17:54:01 +0200

    CRUD on one side completed

--- Lab 2/native/app/src/main/java/com/example/footballmanager/ChampionshipAdapter.java
+++ Lab 2/native/app/src/main/java/com/example/footballmanager/ChampionshipAdapter.java
@@ -1,5 +1,6 @@
 package com.example.footballmanager;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -7,18 +8,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
+import static com.example.footballmanager.Championships.isInteger;
+
 public class ChampionshipAdapter extends BaseAdapter {
     private Context mContext;
     private ArrayList<Championship> championships = new ArrayList<>();
+    private EditText inputMatches;
+    private EditText inputTrophy;
 
-    public ChampionshipAdapter(Context context, ArrayList<Championship> championships) {
+    public ChampionshipAdapter(Context context, ArrayList<Championship> championships, EditText e1, EditText e2) {
         this.mContext = context;
         this.championships = championships;
+        this.inputMatches = e1;
+        this.inputTrophy = e2;
 
     }
 
@@ -38,6 +47,7 @@ public class ChampionshipAdapter extends BaseAdapter {
         return position;
     }
 
+    @SuppressLint("DefaultLocale")
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
@@ -48,16 +58,36 @@ public class ChampionshipAdapter extends BaseAdapter {
         TextView tvTrophy = (TextView) convertView.findViewById(R.id.tvTrophy);
         TextView tvMatches = (TextView) convertView.findViewById(R.id.tvMatches);
         Button btnDelete = (Button) convertView.findViewById(R.id.deleteIItemButton);
+        Button btnUpdate = (Button) convertView.findViewById(R.id.btnUpdate);
+        Button btnPreview = (Button) convertView.findViewById(R.id.btnPreview);
 
 
         tvTrophy.setText(tempChampionship.getTrophy());
-        tvMatches.setText(tempChampionship.getTotalMatches() + " matches");
+        tvMatches.setText(String.format("%d matches", tempChampionship.getTotalMatches()));
 
         btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 championships.remove(tempChampionship);
                 notifyDataSetChanged();
+            }
+        });
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String trophyName = inputTrophy.getText().toString();
+                String matchesNr = inputMatches.getText().toString();
+                boolean isInteger = isInteger(matchesNr);
+                if (!trophyName.equals("") && !matchesNr.equals("") && isInteger) {
+                    tempChampionship.setTotalMatches(Integer.parseInt(matchesNr));
+                    tempChampionship.setTrophy(trophyName);
+                    notifyDataSetChanged();
+                } else {
+                    Toast errorToast = Toast.makeText(mContext, "Trophy and Nr of matches must not be blank and nr of matches must be an int", Toast.LENGTH_SHORT);
+                    errorToast.show();
+                }
+
             }
         });
 
--- Lab 2/native/app/src/main/java/com/example/footballmanager/Championships.java
+++ Lab 2/native/app/src/main/java/com/example/footballmanager/Championships.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +21,31 @@ public class Championships extends AppCompatActivity {
     private Button Create;
     private Button Delete;
     private ListView championshipsList;
+    ChampionshipAdapter myChampionshipAdapter;
+
+    public static boolean isInteger(String str) {
+        if (str == null) {
+            return false;
+        }
+        int length = str.length();
+        if (length == 0) {
+            return false;
+        }
+        int i = 0;
+        if (str.charAt(0) == '-') {
+            if (length == 1) {
+                return false;
+            }
+            i = 1;
+        }
+        for (; i < length; i++) {
+            char c = str.charAt(i);
+            if (c < '0' || c > '9') {
+                return false;
+            }
+        }
+        return true;
+    }
 
 
     private void onInit() {
@@ -27,10 +53,10 @@ public class Championships extends AppCompatActivity {
         Championship ch2 = new Championship(12, "Liga Studentilor UTCN");
         championships.add(ch1);
         championships.add(ch2);
-        for (int i = 12; i <= 18; i += 2) {
-            championships.add(new Championship(i, "Liga " + i));
-        }
-        System.out.println(championships.toString());
+//        for (int i = 12; i <= 18; i += 2) {
+//            championships.add(new Championship(i, "Liga " + i));
+//        }
+//        System.out.println(championships.toString());
     }
 
     @Override
@@ -43,6 +69,8 @@ public class Championships extends AppCompatActivity {
         championshipsList = (ListView) findViewById(R.id.listViewChampionships);
         final EditText inputMatches = (EditText) findViewById(R.id.matchesInput);
         final EditText inputTrophy = (EditText) findViewById(R.id.trophyInput);
+        myChampionshipAdapter = new ChampionshipAdapter(Championships.this, this.championships, inputMatches, inputTrophy);
+
 
         List.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,15 +82,23 @@ public class Championships extends AppCompatActivity {
         Create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.v("tropyName" ,inputTrophy.getText().toString());
-                Log.v("matches" ,inputMatches.getText().toString());
+                String trophyName = inputTrophy.getText().toString();
+                String matchesNr = inputMatches.getText().toString();
+                boolean isInteger = isInteger(matchesNr);
+                if (!trophyName.equals("") && !matchesNr.equals("") && isInteger) {
+
+                    championships.add(new Championship(Integer.parseInt(matchesNr), trophyName));
+                    myChampionshipAdapter.notifyDataSetChanged();
+                } else {
+                    Toast errorToast = Toast.makeText(Championships.this, "Trophy and Nr of matches must not be blank and nr of matches must be an int", Toast.LENGTH_SHORT);
+                    errorToast.show();
+                }
             }
         });
 
     }
 
     public void fillListView() {
-        ChampionshipAdapter myChampionshipAdapter = new ChampionshipAdapter(Championships.this, this.championships);
         championshipsList.setAdapter(myChampionshipAdapter);
     }
 }
