ec138f2be84a251e30edd30497722f8f01a5f07e
Victor-Alexandru <victor21cuciureanu@gmail.com>
2019-12-09 12:53:56 +0200

    all work

--- Server/team/models.py
+++ Server/team/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Team(models.Model):
+
+
+class Championship(models.Model):
+    total_matches = models.IntegerField(null=True, blank=True)
+    trophy = models.TextField(null=True, blank=True)
+
+
