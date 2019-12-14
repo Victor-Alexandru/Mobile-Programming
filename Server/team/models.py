148a14dc2274580464be77628ab51ab68755a548
Victor-Alexandru <victor21cuciureanu@gmail.com>
2019-12-14 20:30:47 +0200

    first changes

--- Server/team/models.py
+++ Server/team/models.py
@@ -4,10 +4,11 @@ from django.db import models
 # Create your models here.
 
 class Team(models.Model):
+    name = models.TextField(null=True, blank=True)
+    matches_played = models.IntegerField()
+    championship = models.ForeignKey(Championship, on_delete=models.CASCADE)
 
 
 class Championship(models.Model):
     total_matches = models.IntegerField(null=True, blank=True)
     trophy = models.TextField(null=True, blank=True)
-
-
