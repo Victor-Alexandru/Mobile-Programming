6c1ea420184ee6db9e1baaf41c953dc59f5ff76e
Victor-Alexandru <victor21cuciureanu@gmail.com>
2019-12-17 14:10:51 +0200

    flutter things

---
+++ flutter_new_pc/football_pc/android/app/src/main/kotlin/com/footballviena/football_pc/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.footballviena.football_pc
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
